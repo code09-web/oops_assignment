11cc0e174fdd50fca9f6aab5c1ce3f4fc818b0dc
Devendra sahu <devendrasahu0309@gmail.com>
2022-08-14 11:46:31 +0530

    1 – Aggregation and Encapsulation

--- oops_assignment/src/com/oops/TestCustomer.java
+++ oops_assignment/src/com/oops/TestCustomer.java
@@ -7,7 +7,21 @@ public class TestCustomer {
 		Address residentialAddress = new Address("Ist Main HSR Layout", "Bangalore");
 		customer.setCustomerName("Jone");
 		customer.setResidentialAddress(residentialAddress);
-		System.out.println(customer.getCustomerName() + " , " + customer.getResidentialAddress().getAddressLine() + " "+ customer.getResidentialAddress().getCity());
+		/*
+		 * • Create an object of Customer with default constructor. Use setters to
+		 * assign values to the member variables. Display the values of member variables
+		 * using getters.
+		 */
+
+		System.out.println(customer.getCustomerName() + " , " + customer.getResidentialAddress().getAddressLine() + " "
+				+ customer.getResidentialAddress().getCity());
+
+		/*
+		 * • Create an object of Customer using parameterized constructor and display
+		 * all the details of the customer using getCustomerDetails method
+		 */
+
+		System.out.println(new Customer("jone",residentialAddress).getCustomerDetails());
 
 	}
 
